26f1e8667f21f2f0ab2501f65b8e526dbcb6fbf2
Rusich <RusichViz@gmail.com>
2021-05-28 19:46:48 +0300

    Changed models

--- foodgram/recipes/models.py
+++ foodgram/recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 User = get_user_model()
@@ -23,20 +24,27 @@ class Ingredient(models.Model):
 
 class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.DO_NOTHING)
-    amount = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField(validators=(
+                                   MinValueValidator(1),
+                                   MaxValueValidator(10000)))
 
     def __str__(self):
         return self.ingredient.title
 
 
 class Recipe(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='recipes')
     title = models.CharField(max_length=256)
     description = models.TextField()
     ingredients = models.ManyToManyField(RecipeIngredient)
-    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING)
-    cooking_time = models.PositiveIntegerField()
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING,
+                            related_name='recipes')
+    cooking_time = models.PositiveIntegerField(validators=(
+                                   MinValueValidator(1),
+                                   MaxValueValidator(2000)))
+    pub_date = models.DateTimeField('date published', auto_now_add=True,
+                                    db_index=True)
     image = models.ImageField(
         upload_to='recipes/',
         verbose_name='Изображение',
