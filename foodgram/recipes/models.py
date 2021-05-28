466e8eb4a3fe393cb4b022c0a28693fea6181ca7
Rusich <RusichViz@gmail.com>
2021-05-28 19:29:14 +0300

    Added models

--- foodgram/recipes/models.py
+++ foodgram/recipes/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=16)
+    color = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=128)
+    dimension = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.DO_NOTHING)
+    amount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.ingredient.title
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    ingredients = models.ManyToManyField(RecipeIngredient)
+    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING)
+    cooking_time = models.PositiveIntegerField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    image = models.ImageField(
+        upload_to='recipes/',
+        verbose_name='Изображение',
+        help_text='Загрузите изображение (необязательно)',
+        blank=True,
+        null=True)
+
+    def __str__(self):
+        return self.title
