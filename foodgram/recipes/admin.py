b9cae1a66086d4937380264fa4541956961d6edf
Rusich <RusichViz@gmail.com>
2021-05-28 20:07:57 +0300

    Added admin models

--- foodgram/recipes/admin.py
+++ foodgram/recipes/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Tag, Ingredient, RecipeIngredient, Recipe
 
-# Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'color')
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'dimension')
+    search_fields = ('title',)
+    list_filter = ('title',)
+    empty_value_display = '-пусто-'
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'ingredient', 'amount')
+    search_fields = ('ingredient',)
+    list_filter = ('ingredient',)
+    empty_value_display = '-пусто-'
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'author', 'tag',
+                    'cooking_time', 'pub_date', 'ingredients')
+    search_fields = ('title',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
