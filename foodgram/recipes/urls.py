f1f64f9523a8a725e7d9740aac5503770524171a
Rusich <RusichViz@gmail.com>
2021-06-07 13:51:32 +0300

    Added author_profile template and view

--- foodgram/recipes/urls.py
+++ foodgram/recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, SubscriptionsView, FavoriteView, favorite_add, favorite_remove
+from .views import IndexView, SubscriptionsView, FavoriteView, favorite_add, favorite_remove, AuthorView
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('my-follow/', FavoriteView.as_view(), name='favorite_recipes'),
     path('favorites/<int:recipe_id>', favorite_remove, name='removeFavorite'),
     path('favorites', favorite_add, name='addFavorite'),
+    path('<str:username>/', AuthorView.as_view(), name='profile'),
 ]
--- foodgram/recipes/views.py
+++ foodgram/recipes/views.py
@@ -3,28 +3,48 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Recipe, User, Favorite
+from .models import Recipe, User, Favorite, Subscription
 from .models import Tag
 
 
 class IndexView(ListView):
-    model = Recipe
     template_name = 'index.html'
     paginate_by = 6
 
     def get_queryset(self):
         tag = self.kwargs.get('tag', )
-        object_list = self.model.objects.all()
+        object_list = Recipe.objects.all()
         if tag:
             object_list = object_list.filter(title__icontains=tag)
         return object_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tag_list'] = Tag.objects.all()
         if self.request.user.is_authenticated:
+            user = self.request.user
+            context['tag_list'] = Tag.objects.all()
+            context['subscription_list'] = Subscription.objects.filter(
+                user=user)
             context['favorite_recipes'] = Recipe.objects.filter(
-                favorites__user=self.request.user)
+                favorites__user=user)
+        return context
+
+
+class AuthorView(IndexView):
+    template_name = 'authorRecipe.html'
+
+    def get_queryset(self):
+        author = get_object_or_404(User, username=self.kwargs['username'])
+        tag = self.kwargs.get('tag', )
+        object_list = Recipe.objects.filter(author=author)
+        if tag:
+            object_list = object_list.filter(title__icontains=tag)
+        return object_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        author = get_object_or_404(User, username=self.kwargs['username'])
+        context['author'] = author
         return context
 
 
