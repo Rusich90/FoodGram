667ada4668fdec27eea5a0c3607962c853f9bcdb
Руслан Алимов <rusichviz@gmail.com>
2021-06-20 22:01:17 +0300

    Added data file

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 SITE_ID = 1
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 DATABASES = {
